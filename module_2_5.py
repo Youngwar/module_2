8752d307726dc21865bb15ed1275d6b7cfad91dd
Youngwar <i.bugaev@zavodpriborov.com>
2024-08-01 21:19:08 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,16 @@
+def get_matrix():
+    n = input('Введите количество строк', )
+    m = input('Введите количество столбцов', )
+    value = input('Введите значение', )
+    matrix = []
+    for i in range(int(n)):
+        matrix.append([])
+
+        for k in range(int(m)):
+            matrix[i].append(value)
+            continue
+
+
+    return matrix
+print(get_matrix())
+
